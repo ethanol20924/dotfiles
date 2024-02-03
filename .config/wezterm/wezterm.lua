ff06f7d657feb0f32f87a375366fc9eac06ee37c
Ethan Lo <ethanlo2010@gmail.com>
2024-02-03 17:55:44 +0100

    add dotfiles

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Mocha'
+
+-- Font
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+
+-- Transparency
+config.window_background_opacity = 0.85
+config.text_background_opacity = 1.0
+
+-- and finally, return the configuration to wezterm
+return config
