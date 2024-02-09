ffce624f154ef5b6128ca6999f1e60984a2ba7b7
Ethan Lo <ethanlo2010@gmail.com>
2024-02-09 12:46:59 +0100

    symlink for wezterm works now?

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 check_existing () {
 	if [ -d "$HOME/$1" ]; then
 		echo "$1 exists, moving to bak/"
+		if [ -d "bak/$1" ]; then
+			rm -rf "bak/$1"
+		fi
 		mkdir -p "bak/$1"; mv -f "$HOME/$1" "$_"
 	fi
 }
@@ -12,6 +15,6 @@ for file in .config/* ;do
 	# call stow
 	stow .
 
-	# manually copy wezterm configs
-	mkdir -p "$HOME/.config/wezterm"; cp .config/wezterm/wezterm.lua "$_"
+	# # manually copy wezterm configs
+	# mkdir -p "$HOME/.config/wezterm"; cp .config/wezterm/wezterm.lua "$_"
 done
