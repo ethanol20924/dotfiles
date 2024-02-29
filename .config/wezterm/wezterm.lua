a0ea359d638a8230291a206ec52c56c5552d09e0
Ethan Lo <ethan.lo@uqconnect.edu.au>
2024-02-29 23:01:23 +0100

    add iterm and firefox configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -26,6 +26,37 @@ config.font = wezterm.font('JetBrainsMono Nerd Font Mono', {weight = 'Bold'})
 config.window_background_opacity = 0.85
 config.text_background_opacity = 1.0
 
+-- Tab bar
+
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'JetBrainsMono Nerd Font Mono', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 12.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
+
+config.colors = {
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
+
 -- Fix for reloading symlinked config
 wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
 
